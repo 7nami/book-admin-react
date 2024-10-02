17bd1389b12109a3eee6c5a335ee85f018be685b
86159 <welovexyy@gmail.com>
2024-10-02 16:16:30 +0800

    Initial commit

---
+++ src/pages/book/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Image from "next/image";
+import localFont from "next/font/local";
+import styles from "@/styles/Home.module.css";
+
+
+
+export default function Home() {
+  return (
+    <>
+      <main className={styles.main}>
+        book
+      </main>
+    </>
+  );
+}
